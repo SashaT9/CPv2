6307a274b84e8a14afecc72529be274fed94d49d
SashaT9 <oleksandr.tymkovych@student.uj.edu.pl>
2024-09-04 00:14:30 +0200

    gen data for topics

---
+++ SQL/fill-tables/gen_topics.sql
@@ -0,0 +1,12 @@
+insert into topics(topic_name) values
+('combinatorics'),
+('recurrences'),
+('graphs'),
+('graph coloring'),
+('numer theory'),
+('constructive'),
+('long output'),
+('calculus'),
+('equations'),
+('geometry'),
+('yes/no');
