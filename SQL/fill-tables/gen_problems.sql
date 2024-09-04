ae6420f1a54b47aa78251157c64a5b2ef2c7aaf7
SashaT9 <oleksandr.tymkovych@student.uj.edu.pl>
2024-09-04 12:12:35 +0200

    added some problems

---
+++ SQL/fill-tables/gen_problems.sql
@@ -0,0 +1,19 @@
+insert into problems(statement, answer, output_only) values
+('find the number of primes up to 10', '4', 'true'),
+('find the value of binom(5, 0)+binom(5, 1)+...+binom(5, 5)', '32', 'true'),
+('what is the value of 0xor1xor2xor...xor100?', '100', 'true'),
+('is that true, that the 3-regular graph with hamiltonian cycle is edge-three-colorable?', 'yes', 'true'),
+('what is the determinant of the matrix
+3 0 0
+9 2 0
+8 8 8
+?', '48', 'true'),
+('find the value of 1+2+3+4+...+50', '1275', 'yes'),
+('check whether the grid graph with vertex a connected to all other vertices is planar', 'no', 'true'),
+('how many permutations on 6 elements with one cycle exists?', '120', 'true'),
+('does finding the hamiltonian path is NP hard?', 'yes', 'true'),
+('find the Ramsey number R(3, 3)', '6', 'true'),
+('is that true that x(G) > col(G)?', 'no', 'true'),
+('what is the maximal value of the б(G), if the planar graph has no triangles', '3', 'true'),
+('what is the maximal value of the б(G)', '5', 'true'),
+('count the number of labeled trees on 4 vertices', '16', 'true');
