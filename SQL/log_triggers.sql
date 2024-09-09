1d396700537c7a47c26158a8bd4dff1a6ee08f12
SashaT9 <oleksandr.tymkovych@student.uj.edu.pl>
2024-09-09 11:42:40 +0200

    some triggers for updating problem_history

--- SQL/log_triggers.sql
+++ SQL/log_triggers.sql
@@ -42,9 +42,35 @@ after update on users
 for each row
 execute function user_log_after_update();
 
-create or replace function user_log_after_solved_problem()
+create or replace function new_problem_added()
 returns trigger as $$
 begin
-
+    insert into problem_history(problem_id, date_of_change, description)
+    values (new.problem_id, current_timestamp, 'problem created with id = ' || new.problem_id);
+    return new;
 end;
-$$
+$$ language plpgsql;
+create or replace trigger new_problem_added_trigger
+after insert on problems
+for each row
+execute function new_problem_added();
+
+create or replace function problem_updated()
+returns trigger as $$
+begin
+    if (new.statement is distinct from old.statement) then
+        insert into problem_history(problem_id, date_of_change, description)
+        values (new.problem_id, current_timestamp, 'problem statement changed from "' || old.statement || '" to "' || new.statement || '"');
+    end if;
+
+    if (new.statement is distinct from old.statement) then
+        insert into problem_history(problem_id, date_of_change, description)
+        values (new.problem_id, current_timestamp, 'problem answer changed from "' || old.answer || '" to "' || new.answer || '"');
+    end if;
+    return new;
+end;
+$$ language plpgsql;
+create or replace trigger problem_updated_trigger
+after update on problems
+for each row
+execute function problem_updated();
